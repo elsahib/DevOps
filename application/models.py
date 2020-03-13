fb05fef5ef4ee1c19c6485b46cfc9c1ebd0c8960
elsahib20 <elsahib20@gmail.com>
2020-03-13 16:08:54 +0000

    update models.py

--- application/models.py
+++ application/models.py
@@ -1,5 +1,7 @@
 from application import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(id):
@@ -10,7 +12,8 @@ class Players(db.Model):
     player_id = db.Column(db.Integer, primary_key=True)
     player_name = db.Column(db.String(30), nullable=False)
     player_age = db.Column(db.Integer, nullable=False)
-    player_team = db.Column(db.String(100), nullable=False)
+    player_team = db.Column(db.String(30), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
   
 
     def __repr__(self):
@@ -19,16 +22,31 @@ class Players(db.Model):
             'Team: ', self.player_team, '\r\n', self.player_id
             ])
 
-class Stats(db.Model, UserMixin):
+class Stats(db.Model):
     stat_id = db.Column(db.Integer, primary_key=True)
-    player_id = db.Column(db.String(500), nullable=False, unique=True)
-    goals = db.Column(db.String(500), nullable=False)
-    assists = db.Column
-    chances = db.Column
-    shots = db.Column
-    minutes = db.Column
-    date = db.Column
+    player_id = db.Column(db.Integer,db.ForeignKey('players.player_id'), nullable=False)
+    goals = db.Column(db.Integer, nullable=False, default=0)
+    assists = db.Column(db.Integer, nullable=False, default=0)
+    chances = db.Column(db.Integer, nullable=False, default=0)
+    shots = db.Column(db.Integer, nullable=False, default=0)
+    minutes = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
 
     
     def __repr__(self):
         return ''.join(['UserID: ', str(self.id), '\r\n', 'Email: ', self.email])
+
+class Users(db.Model, UserMixin):
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
+    player = db.relationship('Players', backref='author', lazy=True)
+
+    def __repr__(self):
+        return ''.join([
+            'User ID: ', str(self.id), '\r\n',
+            'Email: ', self.email, '\r\n',
+            'Name: ', self.first_name, ' ', self.last_name
+        ])
